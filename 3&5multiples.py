483544e83a82d17081a80f95938bdafef61653b9
Drakula01 <50473400+Drakula01@users.noreply.github.com>
2023-08-03 02:16:35 +0530

    Add files via upload

---
+++ 3&5multiples.py
@@ -0,0 +1,10 @@
+def sumof3n5(num):
+    #num = input("Enter a number:")
+    #num=int(num)
+    sum=0
+    for i in range(0,num):
+        if i%3==0 or i%5==0:
+            sum += i
+    print(sum)
+
+sumof3n5(10)
