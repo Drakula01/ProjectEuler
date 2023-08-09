651e976c62e4205552c86a1e6739fa72671af6dc
Drakula01 <50473400+Drakula01@users.noreply.github.com>
2023-08-09 17:55:13 +0530

    Update Problem_4.py

--- Problem_4.py
+++ Problem_4.py
@@ -4,9 +4,7 @@ num=int(num)
 x = 0
 max_pal = 0
 for i in reversed(range(10**(num-1),10**num)):
-    #print('**check i**')
     for j in reversed(range(10**(num-1),10**num)):
-       # print('**check j**')
         prod=str(i*j)
         if prod == prod[::-1]:
             if i*j > max_pal:
